c88ea406c6716ba1785529d39ce209de38e0aaa7
vagrant <vagrant@kleer-csd-box>
2017-01-25 19:19:29 +0100

    palabra

--- model/hangman_lib.py
+++ model/hangman_lib.py
@@ -1,7 +1,33 @@
 class HangMan:
 
     def __init__(self):
-        self.secretWord = "i"
+        self.secretWord = "palabra"
+        self.body = ["O","O","O","O","O","O"]
+        self.partial = ["_","_","_","_","_","_","_"]
+        
     
     def validate(self, word):
-        return word in self.secretWord 
+        i=0
+        is_found = False
+        if (len(word)==1):
+            for letter in self.secretWord:
+                if (letter==word):
+                    self.partial[i] = word
+                    is_found = True
+                i+=1
+            if not is_found:
+                j=0
+                for b in self.body:
+                    if b=='O':
+                        self.body[j]='X'
+                        break
+                    j+=1
+        str_res=""
+        str_b=""
+        for p in self.partial:
+            str_res=str_res+p
+
+        for b in self.body:
+            str_b=str_b+b
+  
+        return str_b, str_res 
--- test/hangman_test.py
+++ test/hangman_test.py
@@ -2,12 +2,31 @@ import unittest
 from model.hangman_lib import HangMan
 
 class TestHangman(unittest.TestCase):
-    def test_oneletter_success(self):
-        hangman = HangMan()
-        result = hangman.validate("i")
-        self.assertTrue(result)
 
-    def test_oneletter_fail(self):
+    def test_one_word_success(self):
         hangman = HangMan()
-        result = hangman.validate("x")
-        self.assertFalse(result)
+        body,partial = hangman.validate("a")
+        self.assertEquals(partial,"_a_a__a")
+        self.assertEquals(body, "OOOOOO")
+ 
+    def test_one_word_success_other(self):
+        hangman = HangMan()
+        body,partial = hangman.validate("p")
+        self.assertEquals(partial,"p______")
+        self.assertEquals( body, "OOOOOO")
+        body,partial = hangman.validate("a")
+        self.assertEquals(partial,"pa_a__a")
+        self.assertEquals( body, "OOOOOO")
+
+    def test_one_word_success_fail(self):
+        hangman = HangMan()
+        body,partial = hangman.validate("i")
+        self.assertEquals( partial,"_______")
+        self.assertEquals( body, "XOOOOO")
+        body,partial = hangman.validate("i")
+        self.assertEquals( partial,"_______")
+        self.assertEquals( body , "XXOOOO")
+
+
+
+
