b731313bcd3a97bf2f990e2704f172398da4370a
vagrant <vagrant@kleer-csd-box>
2017-01-25 15:39:39 +0100

    dev source

---
+++ features/hangman.feature
@@ -0,0 +1,5 @@
+Feature: Hang man
+
+
+Scenario: Check app running
+  Given App is runing
--- test/hangman_test.py
+++ test/hangman_test.py
@@ -1,7 +1,5 @@
 import unittest
-from model.hangman_lib import HangMan
 
 class TestHangman(unittest.TestCase):
     def test_oneNumber(self):
-        hangman = HangMan()
         assert True
